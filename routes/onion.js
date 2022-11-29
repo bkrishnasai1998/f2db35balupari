b7c95f037dd621bda3955504eebf935069d8c4ba
bkrishnasai1998 <59241777+bkrishnasai1998@users.noreply.github.com>
2022-11-29 17:52:18 -0600

    updated code of secure in index.js and login.js

--- routes/onion.js
+++ routes/onion.js
@@ -1,6 +1,17 @@
 var express = require('express');
-const onion_controlers= require('../controllers/onion');
+const onion_controlers = require('../controllers/onion');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', onion_controlers.onion_view_all_Page);
@@ -12,7 +23,7 @@ router.get('/detail', onion_controlers.onion_view_one_Page);
 router.get('/create', onion_controlers.onion_create_Page);
 
 /* GET create update page */
-router.get('/update', onion_controlers.onion_update_Page);
+router.get('/update', secured, onion_controlers.onion_update_Page);
 
 /* GET delete onion page */
 router.get('/delete', onion_controlers.onion_delete_Page);
