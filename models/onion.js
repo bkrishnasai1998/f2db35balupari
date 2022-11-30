6d2c071dd5a10c98db4215481a56e2337690bd86
bkrishnasai1998 <59241777+bkrishnasai1998@users.noreply.github.com>
2022-11-29 20:11:41 -0600

    made code change in onion.js file

--- models/onion.js
+++ models/onion.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
-const onionSchema = mongoose.Schema({
-onion_weight: {type:String, maxLength:10}
+const onionSchema = mongoose.Schema({ 
+onion_weight: {type:String, maxLength:10},
 onion_price: {type:Number,max:100,min:5},
 onion_color: String
 })
